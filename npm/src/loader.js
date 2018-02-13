5b05bf5af4f97a6aa7a8b6242f080ba189743db1
jjyyxx <obfishstudio@gmail.com>
2018-02-13 20:58:56 +0800

    import cache mechanism

--- npm/src/loader.js
+++ npm/src/loader.js
@@ -2,19 +2,42 @@ class WebAudioFontLoader {
     constructor(player) {
         this.player = player
         this.cached = []
+        const req = indexedDB.open('audiofont', 1)
+        this.db = undefined
+        req.onsuccess = () => {
+            this.db = req.result
+        }
+        if (!this.db.objectStoreNames.contains('audiofont')) {
+            this.db.createObjectStore('audiofont', { keyPath: 'variableName' })
+        }
+        this.trans = this.db.transaction(['audiofont'], 'readwrite')
+        this.store = this.trans.objectStore('audiofont')
     }
 
     async load(audioContext, filePath, variableName) {
-        if (window[variableName] || this.cached.indexOf(variableName) !== -1) {
-            return
+        try {
+            return await new Promise((resolve, reject) => {
+                const req = this.store.get(variableName)
+                req.onsuccess = function () {
+                    if (req.result)
+                        resolve(req.result)
+                    else
+                        reject(Error('object not found'))
+                }
+            })
+        } catch (error) {
+            if (this.cached.indexOf(variableName) !== -1) {
+                return
+            }
+            this.cached.push(variableName)
+            const response = await fetch(filePath, {
+                mode: 'cors'
+            })
+            const json = await response.json()
+            this.store.add(json, variableName)
+            this.player.adjustPreset(audioContext, json)
+            return json
         }
-        this.cached.push(variableName)
-        const response = await fetch(filePath, {
-            mode: 'cors'
-        })
-        const json = await response.json()
-        window[variableName] = json
-        this.player.adjustPreset(audioContext, json)
     }
 }
 
